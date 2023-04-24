1022d7cdc5c5509e15fcd4e4a00770d78774e50b
Jose Flores <jjflores255@gmail.com>
2023-04-23 22:57:41 -0400

    Styling on coinPage

--- src/components/CoinPage.js
+++ src/components/CoinPage.js
@@ -2,61 +2,66 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-const CoinPage = ({ coins }) => {
-  // Initialize coin state to an empty object.
+const CoinPage = () => {
+  const params = useParams();
   const [coin, setCoin] = useState({});
-  // Get coinId from URL params.
-  const { coinId } = useParams();
 
-  // useEffect hook to fetch coin data.
+  const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`;
+
   useEffect(() => {
-    // Find the specific coin in the coins array.
-    const specificCoin = coins.find((coin) => coin.id === coinId);
-
-    // If the specific coin is found in the coins array.
-    if (specificCoin) {
-      // Set coin state with the specific coin data
-      // and convert the image property to match the individual coin API data structure.
-      setCoin({
-        ...specificCoin,
-        image: { small: specificCoin.image },
+    axios
+      .get(url)
+      .then((res) => {
+        setCoin(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    } else {
-      // If the specific coin is not found in the coins array,
-      // fetch data from the individual coin API.
-      axios
-        .get(`https://api.coingecko.com/api/v3/coins/${coinId}`)
-        .then((res) => {
-          // Set coin state with the fetched data.
-          setCoin(res.data);
-        })
-        .catch((err) => {
-          // Log the error if there's any.
-          console.log(err);
-        });
-    }
-  }, [coinId, coins]); // Add coinId and coins as dependencies to useEffect.
+  }, [url]);
 
-  // Render the CoinPage component.
   return (
     <div className="container mx-auto px-4 py-8">
       <h2 className="Coin-Name text-2xl font-semibold mb-6 text-center">
         {coin.name}
       </h2>
-      <div className="Coin-Content flex w-full sm:w-60 mb-4 items-center mx-auto">
-        <div className="coin-ranking">
-          <p>Rank #{coin.market_cap_rank}</p>
-        </div>
 
-        <div className="Coin-info text-center items-center flex mx-2">
-          <span className="Coin-heading ">
-            {coin.image ? (
-              <img className="h-[50px]" src={coin.image.small} alt="" />
-            ) : null}
-          </span>
-          <p>{coin.name}</p>
-          <p>{coin.symbol}</p>
-          <p className="coin-price">{coin.current_price}</p>
+      <div className="Coin-Content flex w-full sm:w-60 mb-4 mx-auto">
+        <div className="rows-container flex flex-col">
+          <div className="1st-row text-center items-center flex justify-around mx-2 w-full">
+            <div className="coin-ranking ">
+              <p>Rank #{coin.market_cap_rank}</p>
+            </div>
+
+            <span className="Coin-heading ">
+              {coin.image ? (
+                <img className="h-[50px]" src={coin.image.small} alt="" />
+              ) : null}
+            </span>
+
+            <p>{coin.name}</p>
+            <p>{coin.symbol}</p>
+            <p className="coin-price flex">
+              $
+              {coin.market_data?.current_price ? (
+                <h1>{coin.market_data.current_price.usd.toLocaleString()}</h1>
+              ) : null}
+            </p>
+          </div>
+
+          <div className="second-row">
+            <div className="price-change">
+              <p>{coin.market_data?.price_change_percentage_24h}</p>
+              <p>{coin.market_data?.price_change_percentage_7d}</p>
+              <p>{coin.market_data?.price_change_percentage_30d}</p>
+              <p>{coin.market_data?.price_change_percentage_1y}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="body w-full">
+        <div className="description p-6 ">
+          <p>{coin.description ? coin.description.en : ""}</p>
         </div>
       </div>
     </div>
